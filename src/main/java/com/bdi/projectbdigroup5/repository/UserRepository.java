1a06596fcf7fff740a4962d0366bb8850eb0fe56
Leonie Patricia <patie.leonie@gmail.com>
2024-01-17 16:50:14 +0100

    delete code smells

--- src/main/java/com/bdi/projectbdigroup5/repository/UserRepository.java
+++ src/main/java/com/bdi/projectbdigroup5/repository/UserRepository.java
@@ -3,9 +3,6 @@ package com.bdi.projectbdigroup5.repository;
 import com.bdi.projectbdigroup5.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Iterator;
-import java.util.List;
 import java.util.Optional;
 
 @Repository
