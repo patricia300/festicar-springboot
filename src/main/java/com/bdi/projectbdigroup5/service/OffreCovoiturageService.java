3587ec3c2c74740cce430db3712c5a43a5f5f6c3
Leonie Patricia <patie.leonie@gmail.com>
2024-01-23 13:07:13 +0100

    Create endpoint to get all covoiturage with tri and pagination

--- src/main/java/com/bdi/projectbdigroup5/service/OffreCovoiturageService.java
+++ src/main/java/com/bdi/projectbdigroup5/service/OffreCovoiturageService.java
@@ -1,11 +1,20 @@
 package com.bdi.projectbdigroup5.service;
 
+import com.bdi.projectbdigroup5.model.OffreCovoiturage;
 import com.bdi.projectbdigroup5.repository.OffreCovoiturageRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.Set;
+
 @Service
+@AllArgsConstructor
 public class OffreCovoiturageService {
-    @Autowired
     private OffreCovoiturageRepository offreCovoiturageRepository;
+
+    public Iterable<OffreCovoiturage> getAllOffreCovoiturages(Pageable pageable){
+        return offreCovoiturageRepository.findAll(pageable);
+    }
 }
