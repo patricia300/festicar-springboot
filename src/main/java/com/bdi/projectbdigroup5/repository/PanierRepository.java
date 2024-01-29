9dc73cfdc92681dd61ef8360e6b2f0753efd1fb8
Leonie Patricia <patie.leonie@gmail.com>
2024-01-29 11:30:01 +0100

    get current panier

--- src/main/java/com/bdi/projectbdigroup5/repository/PanierRepository.java
+++ src/main/java/com/bdi/projectbdigroup5/repository/PanierRepository.java
@@ -1,6 +1,7 @@
 package com.bdi.projectbdigroup5.repository;
 
 import com.bdi.projectbdigroup5.model.Panier;
+import com.bdi.projectbdigroup5.model.StatutPanier;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
