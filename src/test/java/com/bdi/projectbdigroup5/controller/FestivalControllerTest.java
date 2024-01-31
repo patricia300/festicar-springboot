09900aaa162f28fa6dc5a9e0cdca8d878dc799c5
Leonie Patricia <patie.leonie@gmail.com>
2024-01-31 11:17:36 +0100

    FestivalControllerTest

---
+++ src/test/java/com/bdi/projectbdigroup5/controller/FestivalControllerTest.java
@@ -0,0 +1,44 @@
+package com.bdi.projectbdigroup5.controller;
+
+import com.bdi.projectbdigroup5.property.PageableProperties;
+import com.bdi.projectbdigroup5.service.FestivalService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(controllers = FestivalController.class)
+class FestivalControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private FestivalService festivalService;
+
+    @MockBean
+    private PageableProperties pageableProperties;
+
+    @Test
+    void getAllFestivals() throws Exception {
+        mockMvc.perform(get("/festivals"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getOneFestival() throws Exception {
+        mockMvc.perform(get("/festivals/1776"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getAllFestivalsByName() throws Exception {
+        mockMvc.perform(get("/festivals/by-nom?nom=vagamondes"))
+                .andExpect(status().isOk());
+
+    }
+}
